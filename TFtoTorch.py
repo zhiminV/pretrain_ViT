ee684e1a36c6f92fecb004adbf15b0e25e26bedb
zhiminV <122182731+zhiminV@users.noreply.github.com>
2024-07-19 16:20:25 -0700

    add file to convert tensorflow to torch

---
+++ TFtoTorch.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset, DataLoader
+from transformers import ViTFeatureExtractor
+import tensorflow as tf
+import torch
+from preprocessData import train_dataset,val_dataset,test_dataset
+
+class TFRecordDataset(Dataset):
+    def __init__(self, tf_dataset):
+        self.tf_dataset = tf_dataset
+        self.data = list(tf_dataset.as_numpy_iterator())
+
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        inputs, labels = self.data[idx]
+        inputs = torch.from_numpy(inputs).permute(2, 0, 1).float()  # 将 (H, W, C) 转换为 (C, H, W)
+        labels = torch.from_numpy(labels).float()
+        return inputs, labels
+
+# 创建 PyTorch 数据集
+train_torch_dataset = TFRecordDataset(train_dataset)
+val_torch_dataset = TFRecordDataset(val_dataset)
+test_torch_dataset = TFRecordDataset(test_dataset)
+
+# 创建 DataLoader
+train_loader = DataLoader(train_torch_dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
+val_loader = DataLoader(val_torch_dataset, batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
+test_loader = DataLoader(test_torch_dataset, batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
--- preprocessData.py
+++ preprocessData.py
@@ -1,58 +1,92 @@
 import re
 from typing import Dict, List, Text, Tuple
 import tensorflow as tf
-import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import ViTFeatureExtractor
 
-# 定义输入和输出特征
+# Constants for data processing
 INPUT_FEATURES = ['elevation', 'th', 'vs', 'tmmn', 'tmmx', 'sph', 'pr', 'pdsi', 'NDVI', 'population', 'erc', 'PrevFireMask']
 OUTPUT_FEATURES = ['FireMask', ]
 
-# 数据统计信息
 DATA_STATS = {
     'elevation': (0.0, 3141.0, 657.3003, 649.0147),
     'pdsi': (-6.12974870967865, 7.876040384292651, -0.0052714925, 2.6823447),
     'NDVI': (-9821.0, 9996.0, 5157.625, 2466.6677),
     'pr': (0.0, 44.53038024902344, 1.7398051, 4.482833),
-    'sph': (0.0, 1.0, 0.0071658953, 0.0042835088),
-    'th': (0.0, 360.0, 190.32976, 72.59854),
+    'sph': (0., 1., 0.0071658953, 0.0042835088),
+    'th': (0., 360.0, 190.32976, 72.59854),
     'tmmn': (253.15, 298.94891357421875, 281.08768, 8.982386),
     'tmmx': (253.15, 315.09228515625, 295.17383, 9.815496),
     'vs': (0.0, 10.024310074806237, 3.8500874, 1.4109988),
     'erc': (0.0, 106.24891662597656, 37.326267, 20.846027),
-    'population': (0.0, 2534.06298828125, 25.531384, 154.72331),
-    'PrevFireMask': (-1.0, 1.0, 0.0, 1.0),
-    'FireMask': (-1.0, 1.0, 0.0, 1.0),
+    'population': (0., 2534.06298828125, 25.531384, 154.72331),
+    'PrevFireMask': (-1., 1., 0., 1.),
+    'FireMask': (-1., 1., 0., 1.)
 }
 
-# 数据裁剪和标准化函数
+def random_crop_input_and_output_images(input_img: tf.Tensor, output_img: tf.Tensor, sample_size: int, num_in_channels: int, num_out_channels: int) -> Tuple[tf.Tensor, tf.Tensor]:
+    combined = tf.concat([input_img, output_img], axis=2)
+    combined = tf.image.random_crop(combined, [sample_size, sample_size, num_in_channels + num_out_channels])
+    input_img = combined[:, :, 0:num_in_channels]
+    output_img = combined[:, :, -num_out_channels:]
+    return input_img, output_img
+
+def center_crop_input_and_output_images(input_img: tf.Tensor, output_img: tf.Tensor, sample_size: int) -> Tuple[tf.Tensor, tf.Tensor]:
+    central_fraction = sample_size / input_img.shape[0]
+    input_img = tf.image.central_crop(input_img, central_fraction)
+    output_img = tf.image.central_crop(output_img, central_fraction)
+    return input_img, output_img
+
+def _get_base_key(key: Text) -> Text:
+    match = re.match(r'([a-zA-Z]+)', key)
+    if match:
+        return match.group(1)
+    raise ValueError('The provided key does not match the expected pattern: {}'.format(key))
+
 def _clip_and_rescale(inputs: tf.Tensor, key: Text) -> tf.Tensor:
-    base_key = re.match(r'([a-zA-Z]+)', key).group(1)
+    base_key = _get_base_key(key)
+    if base_key not in DATA_STATS:
+        raise ValueError('No data statistics available for the requested key: {}.'.format(key))
     min_val, max_val, _, _ = DATA_STATS[base_key]
     inputs = tf.clip_by_value(inputs, min_val, max_val)
     return tf.math.divide_no_nan((inputs - min_val), (max_val - min_val))
 
 def _clip_and_normalize(inputs: tf.Tensor, key: Text) -> tf.Tensor:
-    base_key = re.match(r'([a-zA-Z]+)', key).group(1)
+    base_key = _get_base_key(key)
+    if base_key not in DATA_STATS:
+        raise ValueError('No data statistics available for the requested key: {}.'.format(key))
     min_val, max_val, mean, std = DATA_STATS[base_key]
     inputs = tf.clip_by_value(inputs, min_val, max_val)
     inputs = inputs - mean
     return tf.math.divide_no_nan(inputs, std)
 
-# 数据解析函数
+def _get_features_dict(sample_size: int, features: List[Text]) -> Dict[Text, tf.io.FixedLenFeature]:
+    sample_shape = [sample_size, sample_size]
+    features = set(features)
+    columns = [tf.io.FixedLenFeature(shape=sample_shape, dtype=tf.float32) for _ in features]
+    return dict(zip(features, columns))
+
 def _parse_fn(example_proto, data_size, sample_size, num_in_channels, clip_and_normalize, clip_and_rescale, random_crop, center_crop):
+    if random_crop and center_crop:
+        raise ValueError('Cannot have both random_crop and center_crop be True')
     input_features, output_features = INPUT_FEATURES, OUTPUT_FEATURES
     feature_names = input_features + output_features
-    features_dict = {key: tf.io.FixedLenFeature(shape=[data_size, data_size], dtype=tf.float32) for key in feature_names}
+    features_dict = _get_features_dict(data_size, feature_names)
     features = tf.io.parse_single_example(example_proto, features_dict)
-    
-    inputs_list = [_clip_and_normalize(features[key], key) for key in input_features] if clip_and_normalize else [_clip_and_rescale(features[key], key) for key in input_features]
+
+    if clip_and_normalize:
+        inputs_list = [_clip_and_normalize(features.get(key), key) for key in input_features]
+    elif clip_and_rescale:
+        inputs_list = [_clip_and_rescale(features.get(key), key) for key in input_features]
+    else:
+        inputs_list = [features.get(key) for key in input_features]
+
     inputs_stacked = tf.stack(inputs_list, axis=0)
     input_img = tf.transpose(inputs_stacked, [1, 2, 0])
     
-    outputs_list = [features[key] for key in output_features]
+    outputs_list = [features.get(key) for key in output_features]
+    assert outputs_list, 'outputs_list should not be empty'
     outputs_stacked = tf.stack(outputs_list, axis=0)
     output_img = tf.transpose(outputs_stacked, [1, 2, 0])
 
@@ -63,13 +97,58 @@ def _parse_fn(example_proto, data_size, sample_size, num_in_channels, clip_and_n
 
     return input_img, output_img
 
-# 获取数据集函数
 def get_dataset(file_pattern, data_size, sample_size, batch_size, num_in_channels, compression_type, clip_and_normalize, clip_and_rescale, random_crop, center_crop):
+    if clip_and_normalize and clip_and_rescale:
+        raise ValueError('Cannot have both normalize and rescale.')
+    
     dataset = tf.data.Dataset.list_files(file_pattern)
-    dataset = dataset.interleave(lambda x: tf.data.TFRecordDataset(x, compression_type=compression_type), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.interleave(
+        lambda x: tf.data.TFRecordDataset(x, compression_type=compression_type),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.map(lambda x: _parse_fn(x, data_size, sample_size, num_in_channels, clip_and_normalize, clip_and_rescale, random_crop, center_crop), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.map(
+        lambda x: _parse_fn(x, data_size, sample_size, num_in_channels, clip_and_normalize, clip_and_rescale, random_crop, center_crop),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     return dataset
 
+# 加载数据集
+train_dataset = get_dataset(
+    file_pattern='/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/train/*_ongoing_*.tfrecord',
+    data_size=64,
+    sample_size=64,
+    batch_size=32,
+    num_in_channels=12,
+    compression_type=None,
+    clip_and_normalize=True,
+    clip_and_rescale=False,
+    random_crop=True,
+    center_crop=False
+)
+
+val_dataset = get_dataset(
+    file_pattern='/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/val/*_ongoing_*.tfrecord',
+    data_size=64,
+    sample_size=64,
+    batch_size=32,
+    num_in_channels=12,
+    compression_type=None,
+    clip_and_normalize=True,
+    clip_and_rescale=False,
+    random_crop=True,
+    center_crop=False
+)
+
+test_dataset = get_dataset(
+    file_pattern='/Users/lzm/Desktop/7980 Capstone/rayan 项目/northamerica_2012-2023/test/*_ongoing_*.tfrecord',
+    data_size=64,
+    sample_size=64,
+    batch_size=32,
+    num_in_channels=12,
+    compression_type=None,
+    clip_and_normalize=True,
+    clip_and_rescale=False,
+    random_crop=True,
+    center_crop=False
+)
